ad86897645021746220fd189cc0121965cc81017
Mark <mark.simpson@asset.tv>
2021-01-12 12:14:26 +0000

    Add Test

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Teleprompter Candidate test
 Teleprompter Candidate test
+
+## Setup
+- Fork Repo
+- Set up a new Drupal 9 instance.
+  - [DDEV](https://ddev.readthedocs.io/en/latest/users/cli-usage/#drupal-8-quickstart) is quite easy to set up, otherwise docker.
+- Clone teleprompter into `modules/custom`
+- Enable Teleprompter Module
+  - Ensure dependancies are met: restui, views
+## Essential
+- Update Teleprompter Module
+ - Enable REST on endpoint "teleprompter_rest_resource" and use Basic Auth
+   - Accept a POST request from Authenticated Users
+ - Ensure only alpha characters(Max 255) are present in the "question" field and that the only field present is "question"; otherwise fail the request with 400 and return appropriate message
+    - Format example can be found in /tests/question.json
+ - Create a question entity
+   - Set the Status to "show"
+   - Return a 201 with appropriate message
+- Update Teleprompter View to include the users First name and Surname
+ - Add the changes to config/install
+
+## Bonus
+- Write some appropriate unit tests
+- Update View to automatically refresh every 30 seconds(AJAX)
+- Sanitise the question by making the first character capitalised and the last character is "?" if not present. 
+  - Create a service to perform sanitisation
---
+++ src/Plugin/rest/resource/TeleprompterRestResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\teleprompter\Plugin\rest\resource;
+
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Provides a resource to create a Question Entity.
+ *
+ * @RestResource(
+ *   id = "teleprompter_rest_resource",
+ *   label = @Translation("Teleprompter rest resource"),
+ *   uri_paths = {
+ *     "create" = "/api/v1/teleprompter/question"
+ *   }
+ * )
+ */
+class TeleprompterRestResource extends ResourceBase {
+
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->logger = $container->get('logger.factory')->get('teleprompter');
+    $instance->currentUser = $container->get('current_user');
+    return $instance;
+  }
+
+    /**
+     * Responds to POST requests.
+     *
+     * @param string $payload
+     *
+     * @return \Drupal\rest\ModifiedResourceResponse
+     *   The HTTP response object.
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     *   Throws exception expected.
+     */
+    public function post($payload) {
+
+        // You must to implement the logic of your REST Resource here.
+        // Use current user after pass authentication to validate access.
+        if (!$this->currentUser->hasPermission('access content')) {
+            throw new AccessDeniedHttpException();
+        }
+
+        return new ModifiedResourceResponse($payload, 200);
+    }
+
+}
